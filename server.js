8f2e10a55456fa18205dea6c0308244164f41e5e
Miggy <allamas0726@gmail.com>
2018-08-18 10:39:18 +0800

    Changed file paths

--- server.js
+++ server.js
@@ -46,6 +46,10 @@ app.set("view-engine", "hbs");
 mongoose.Promise = global.Promise;
 
 // connect to the database
+//mongoose.connect("mongodb://localhost:27017/memesdata", {
+//    useNewUrlParser: true 
+//});
+
 mongoose.connect("mongodb://admin, r12345@ds123822.com:23822/ragememes", {
     useNewUrlParser: true 
 });
